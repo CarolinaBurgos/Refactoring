d5412582aefbbba53e94d312d1bf030b9fc1df46
Carolina Burgos <caralbur@espol.edu.ec>
2018-08-04 17:21:20 -0800

    Commentarios

--- Comentarios/src/Correcto/Correcto.java
+++ Comentarios/src/Correcto/Correcto.java
@@ -5,17 +5,42 @@
  */
 package Correcto;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Carolina
  */
 public class Correcto {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+
+        double estatura; 
+        double peso;
+        double indice; 
+
+        System.out.print("Ingrese su peso en kg: ");
+        peso = sc.nextFloat();
+
+        System.out.print("Ingrese su estatura en m: ");
+        estatura = sc.nextFloat();
+
+        indice = indiceMasaCorporal(peso, estatura);
+        System.out.println("\nSu indice de masa corporal es: " + indice);
+
+        if (indice < 18.5) { 
+            System.out.println("\nBAJO PESO");
+        } else if (indice > 18.5 && indice < 24.99) {
+            System.out.println("\nPESO NORMAL");
+        } else if (indice >= 25.00) {
+            System.out.println("\nSOBREPESO");
+        }
     }
-    
+
+    public static double indiceMasaCorporal(double peso, double estatura) {
+        double indice = peso / Math.pow(estatura, 2);
+        return indice;
+    }
+
 }
--- Comentarios/src/Incorrecto/Incorrecto.java
+++ Comentarios/src/Incorrecto/Incorrecto.java
@@ -5,17 +5,51 @@
  */
 package Incorrecto;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Carolina
  */
 public class Incorrecto {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+
+        double est; //Valor de la estatura
+        double p; //Valor del peso 
+        double imc; //Valor del indice de masa corporal
+
+        System.out.print("Ingrese su peso en kg: ");
+        p = sc.nextFloat();
+
+        System.out.print("Ingrese su estatura en m: ");
+        est = sc.nextFloat();
+
+        imc = imc(p, est);
+        System.out.println("\nSu indice de masa corporal es: " + imc);
+
+        if (imc < 18.5) { //Si el indice de masa corporal es menor a 18.5, la persona posee un peso bajo
+            System.out.println("\nBAJO PESO"); 
+        //Si el indice de masa corporal esta entre 18.5 y 24.99, la persona posee un peso normal
+        } else if (imc > 18.5 && imc < 24.99) {
+            System.out.println("\nPESO NORMAL");
+        //Si el indice de masa corporal es menor a 18.5, la persona posee sobrepeso
+        } else if (imc >= 25.00) {
+            System.out.println("\nSOBREPESO");
+        }
     }
     
+    //Metodo que permite el calculo de indice de masa corporal
+    
+    /**
+     * Metodo que permite el calculo de indice de masa corporal
+     * @param p valor del peso
+     * @param e valor de la estatura
+     * @return valor del indice de masa corporal
+     */
+    public static double imc(double p, double e) {
+        return p / Math.pow(e, 2);
+    }
+
 }
