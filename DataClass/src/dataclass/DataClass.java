2abaea76fb57fc77512910630c0a2a8931f37e47
Carolina Burgos <caralbur@espol.edu.ec>
2018-08-04 18:34:54 -0800

    Duplicate Code

---
+++ DataClass/src/dataclass/DataClass.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dataclass;
+
+/**
+ *
+ * @author Carolina
+ */
+public class DataClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
