a630dabe2e4cc8d842d70617bff14e3fdb4ae82f
Carolina Burgos <caralbur@espol.edu.ec>
2018-08-04 21:17:46 -0800

    Data Clumps

---
+++ DataClumps/src/Incorrecto/Incorrecto.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Incorrecto;
+
+/**
+ *
+ * @author Carolina
+ */
+public class Incorrecto {
+
+    public static void main(String args[]) {
+
+        String firstName = "Carolina";
+        String lastName = "Burgos";
+        int age = 22;
+        String gender = "girl";
+        String occupation = "estudiante";
+        String city = "Guayaquil";
+
+        welcomeNew(firstName, lastName, age, gender, occupation, city);
+    }
+
+    public static void welcomeNew(String firstName, String lastName, int age, String gender, String occupation, String city) {
+        System.out.printf("Welcome %s %s, a %d-year-old %s from %s who works as a %s\n", firstName, lastName, age, gender, city, occupation);
+    }
+}
