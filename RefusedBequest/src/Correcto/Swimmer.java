7516004473626b779f83624a1d63462b62a80ab6
Carolina Burgos <caralbur@espol.edu.ec>
2018-08-06 19:26:44 -0800

    Refused Request

---
+++ RefusedBequest/src/Correcto/Swimmer.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Correcto;
+
+public class Swimmer extends Animal {
+    
+    private boolean poseeEscamas;
+
+    public boolean isPoseeEscamas() {
+        return poseeEscamas;
+    }
+    
+}
---
+++ RefusedBequest/src/Incorrecto/Animal.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Incorrecto;
+
+public class Animal {
+
+    private int numberOfLegs;
+    private String name;
+    private String breed;
+
+    public int getNumberOfLegs() {
+        return numberOfLegs;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+    
+    
+    
+}
